43fe2100ae41a7266ff51d219aad1193045fa8f1
Stephane Moore <stephane.moore@gmail.com>
2015-05-29 02:14:21 -0700

    [source templates] Initial import of an Xcode source template installer with some bare templates.
    Tested with Xcode Version 6.3.1 (6D1002).

---
+++ templates/source/Bare ObjC Class.xctemplate/NSObjectObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,9 @@
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif // !defined(__has_feature) || !__has_feature(objc_arc)
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
